e1212b2149c16e37fb141fd23f2f2bcf3be2bd6d
unknown <mschubert@10426-FALC.leap.corp>
2016-04-21 20:19:11 -0700

    Added project.

---
+++ PinchSlowDown/Assets/MyScripts/DualCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DualCamera : MonoBehaviour {
+
+    public Camera myTopDown;
+    public bool isTopDownShowing;
+
+	// Use this for initialization
+	void Start () {
+
+        updatecam();
+	}
+	
+    void updatecam()
+    {
+        myTopDown.enabled = isTopDownShowing;
+        UnityEngine.VR.VRSettings.showDeviceView = !isTopDownShowing;
+    
+    }
+
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.V) )
+        {
+                    isTopDownShowing = !isTopDownShowing;
+            updatecam ();
+        }
+	}
+}
